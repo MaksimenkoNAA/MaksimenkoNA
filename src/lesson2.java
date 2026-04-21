87d17fb3f62a054f1d765e6b73859b93f31b37ef
Максименко Надежда <yaichnikovana@yandex.ru>
2026-04-21 21:29:10 +0400

    DZ2.2-1

---
+++ src/lesson2.java
@@ -0,0 +1,7 @@
+public class lesson2 {
+    public static void main(String[] args) {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+}
