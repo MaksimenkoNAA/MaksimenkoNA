69e7543ec934ae36efdfa90e465b5f7cb846c4bc
Максименко Надежда <yaichnikovana@yandex.ru>
2026-04-22 16:16:14 +0400

    DZ2.2-6

--- src/Lesson8.java
+++ src/Lesson8.java
@@ -1,10 +1,4 @@
 public class Lesson8 {
-    /**
-     * Определяет, является ли переданное число положительным или отрицательным.
-     * Ноль считается положительным числом.
-     *
-     * @param number целое число для проверки
-     */
     public static void printNumberSign(int number) {
         if (number >= 0) {
             System.out.println("Положительное");
@@ -12,9 +6,8 @@ public class Lesson8 {
             System.out.println("Отрицательное");
         }
     }
-
     public static void main(String[] args) {
         // Тестовые случаи
-        printNumberSign(67);   // Положительное (число > 0)
+        printNumberSign(67);
     }
 }
