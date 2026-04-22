5fdd3bb0374e39dd91f838eddcae3a0f06ea3185
Максименко Надежда <yaichnikovana@yandex.ru>
2026-04-22 16:07:54 +0400

    DZ2.2-2

--- src/Lesson4.java
+++ src/Lesson4.java
@@ -1,15 +1,16 @@
 public class Lesson4 {
     public static void checkSumSign() {
-        int a = 65;  // инициализация переменной a
-        int b = -11;  // инициализация переменной b
-        int sum = a + b;  // вычисление суммы
+        int a = 65;
+        int b = -11;
+        int sum = a + b;
         if (sum >= 0) {
             System.out.println("Сумма положительная");
         } else {
             System.out.println("Сумма отрицательная");
         }
     }
+
     public static void main(String[] args) {
-        checkSumSign();  // вызов метода
+        checkSumSign();
     }
 }
