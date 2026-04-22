b9a10059de52e0f275f388e06fa29d33813d1970
Максименко Надежда <yaichnikovana@yandex.ru>
2026-04-22 12:41:33 +0400

    DZ2.2-4

---
+++ src/Lesson6.java
@@ -0,0 +1,14 @@
+public class Lesson6 {
+    public static void compareNumbers() {
+        int a = 33;  // инициализация переменной a
+        int b = 26;  // инициализация переменной b
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+    public static void main(String[] args) {
+        compareNumbers();  // вызов метода
+    }
+}
