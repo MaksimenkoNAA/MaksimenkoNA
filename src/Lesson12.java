c9fbaa73a53cb3c7acb64c7e746c8ed42c8c844d
Максименко Надежда <yaichnikovana@yandex.ru>
2026-04-22 15:18:48 +0400

    DZ2.2-10

---
+++ src/Lesson12.java
@@ -0,0 +1,20 @@
+public class Lesson12 {
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 0, 0, 1, 1, 1, 1, 1, 0};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                arr[i] = 1;
+            } else {
+                arr[i] = 0;
+            }
+        }
+        System.out.print("[");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i < arr.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+    }
+}
