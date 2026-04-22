7bab05565d4efd0997c9eeaeae416b26c51aa2f4
Максименко Надежда <yaichnikovana@yandex.ru>
2026-04-22 15:24:28 +0400

    DZ2.2-11

---
+++ src/Lesson13.java
@@ -0,0 +1,11 @@
+public class Lesson13 {
+    public static void main(String[] args) {
+    int[] arr = new int[100];
+    for (int i = 0; i < arr.length; i++) {
+        arr[i] = i + 1;
+    }
+    for (int i = 0; i < arr.length; i++) {
+        System.out.print(arr[i] + " ");
+    }
+    }
+}
