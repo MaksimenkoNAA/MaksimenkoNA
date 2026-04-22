06070baca73b3a44b0f71a8768b2414c9b97285c
Максименко Надежда <yaichnikovana@yandex.ru>
2026-04-22 15:35:39 +0400

    DZ2.2-13

---
+++ src/Lesson15.java
@@ -0,0 +1,15 @@
+public class Lesson15 {
+    public static void main(String[] args) {
+        int size = 3;
+        int[][] array = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            array[i][i] = 1;
+        }
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
