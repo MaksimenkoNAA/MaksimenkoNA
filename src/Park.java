3dc2e1a0b0a32b7020eab9edd1ceb3b5a87ada10
Максименко Надежда <yaichnikovana@yandex.ru>
2026-04-24 16:52:08 +0400

    DZ2.3-3

---
+++ src/Park.java
@@ -0,0 +1,10 @@
+public class Park {
+    private String название;
+    private Аттракцион аттракцион;
+    public class Аттракцион {
+        public String названиеАттракциона;
+        public String времяРаботы;
+        public double стоимость;
+    }
+}
+
